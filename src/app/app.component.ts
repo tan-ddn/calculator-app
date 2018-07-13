567568c32c96c8f06c7688e07fdb54c5fee98d78
Tan <tan@bluesparc.com.sg>
2018-07-13 13:38:55 +0800

    Stage 2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Calculator App';
+  num1: number;
+  num2: number;
+  result: number;
+
+  currentNum: number = 0;
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  substract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 / this.num2;
+  }
+
+  getInput(event: any) {
+    this.currentNum = event.target.value;
+  }
 }
