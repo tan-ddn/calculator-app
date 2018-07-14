0366437abce01ca74e5a23632ea439528ccba532
Tan <tan.dd.nguyen@gmail.com>
2018-07-14 20:28:40 +0800

    Basic operations: + - * / AC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,78 @@ export class AppComponent {
   title = 'Calculator App';
   num1: number;
   num2: number;
-  result: number;
 
+  previousNum: number = 0;
   currentNum: number = 0;
+  currentInput: string = '0';
+  operation: string = '+';
+  result: number = 0;
+
+  countDecimals(a: number): number {
+    if (Math.floor(a) === a) return 0;
+    return a.toString().split('.')[1].length || 0;
+  }
+  getLongestDecimals(a:number, b: number): number {
+    return (this.countDecimals(a) > this.countDecimals(b)) ? this.countDecimals(a) : this.countDecimals(b);
+  }
   add() {
-    this.result = this.num1 + this.num2;
+    this.result = Number.parseFloat((this.num1 + this.num2).toFixed(this.getLongestDecimals(this.num1, this.num2)));
   }
   substract() {
-    this.result = this.num1 - this.num2;
+    this.result = Number.parseFloat((this.num1 - this.num2).toFixed(this.getLongestDecimals(this.num1, this.num2)));
   }
   multiply() {
-    this.result = this.num1 * this.num2;
+    this.result = this.num1 * this.num2;  
   }
   divide() {
-    this.result = this.num1 / this.num2;
+    this.result = this.num1 / this.num2;  
   }
 
-  getInput(event: any) {
-    this.currentNum = event.target.value;
+  allClear() {
+    this.previousNum = 0;
+    this.currentNum = 0;
+    this.currentInput = '0';
+    this.operation = '+';
+    this.result = 0;
   }
+  getInput(event: any) {
+    //console.log(isNaN(event.target.value)+' '+Number.isNaN(event.target.value));
+    if (event.target.value === '.' && this.currentInput.indexOf('.') !== -1) {
+      return;
+    }
+    if (this.currentInput === '0' || this.result !== 0) {
+      this.result = 0;
+      this.currentInput = (event.target.value === '.') ? '0' + event.target.value : event.target.value; 
+    } else {
+      this.currentInput += event.target.value;
+    }
+    this.currentNum = Number.parseFloat(this.currentInput);
+    console.log(this.currentInput+' '+this.currentNum);
+  }
+  doOperation(event: any) {
+    this.getResult();
+    this.previousNum = this.result;
+    this.currentInput = '0';
+    this.operation = event.target.value;
+  }
+  getResult() {
+    this.num1 = this.previousNum;
+    this.num2 = this.currentNum;
+    switch (this.operation) {
+      case '+':
+        this.add();
+        break;
+      case '-':
+        this.substract();
+        break;
+      case 'X':
+        this.multiply();
+        break;
+      case '/':
+        this.divide();
+        break;
+    }
+    this.currentInput = this.result.toString();
+  }
+
 }
